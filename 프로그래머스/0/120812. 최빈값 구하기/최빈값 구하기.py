b04cf314cb65f4fb2f6aa5a8136baa5f99c2d064
JiWon <43868373+JiWon9709@users.noreply.github.com>
2025-04-04 10:28:37 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.18 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,26 +1,16 @@
 def solution(array):
     answer = []
     cnt = []
+    # 1 2 3 3 3 4
+    # (1, 1)
+    # (2, 1)
+    # (3, 1)
+    # (3, 2)
+    # (3, 3)
     for i in range(len(array)):
         if array[i] not in answer:
-            answer.append(array[i])
-            cnt.append(1)
+            answer.append([array[i], 1])
         else:
-            for j in range(len(answer)):
-                if answer[j] == array[i]:
-                    cnt[j] += 1
-                    break
-    
-    max_idx = 0
-    cnt_idx = 0 # 최빈값 개수를 세기 위한 변수
-    for i in range(len(cnt)):
-        if cnt[max_idx] < cnt[i]:
-            max_idx = i
-            cnt_idx = 1
-        elif cnt[max_idx] == cnt[i]:
-            cnt_idx += 1
-        
-    if cnt_idx > 1:
-        return -1
-    else:
-        return answer[max_idx]
+            answer[array[i]][1] += 1
+    idx = max(answer[1])
+    return answer
