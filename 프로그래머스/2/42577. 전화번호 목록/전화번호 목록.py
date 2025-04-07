cb8e9f7a4c9519b4f7f7c4bc4b6cd7a8d190c888
JiWon <43868373+JiWon9709@users.noreply.github.com>
2025-04-07 17:56:03 +0900

    [level 2] Title: 전화번호 목록, Time: 449.36 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,30 +1,15 @@
 def solution(phone_book):
     answer = True
-    # hash: 해시함수를 통해 특정값을 저장하는 방법
-    phone_book.sort()
-    book = {}
+    hash_map = {}
+    # phone_book을 해시맵에 저장
     for i in phone_book:
-        if i[0] in book:
-            book[i[0]].append(i)
-        else:
-            book[i[0]] = [i]
-    # 앞자리가 같은거끼리만 비교
-    for lst in book.values():
-        length = len(lst)
-        if length >= 2:
-            for i in range(length-1):
-                if lst[i] == lst[i+1][:len(lst[i])]:
-                    return False
-    # for i in range(len(phone_book)-1):
-    #     for j in range(i+1, len(phone_book)):
-    #         if phone_book[i][0] == phone_book[j][0]:
-    #             for k in range(len(phone_book[i])):
-    #                 if phone_book[i][k] != phone_book[j][k]:
-    #                     break
-    #             else:
-    #                  return False   
-    #         else:
-    #             break
-            # if phone_book[i] == phone_book[j][:len(phone_book[i])]:
-            #     return False
+        hash_map[i] = 1
+    for phone_number in phone_book:
+        tmp = ""
+        # 해시맵에 저장되어있는 번호의 숫자 하나씩 가져오기
+        for number in phone_number:
+            tmp += number # tmp에 하나씩 넣어서 있는지 확인
+            # print("phone_number:",phone_number,"tmp:", tmp)
+            if tmp in hash_map and tmp != phone_number:
+                return False
     return answer
