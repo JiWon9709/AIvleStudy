051cae0a247bffc8eaa10983754f2da1a59b83b6
JiWon <43868373+JiWon9709@users.noreply.github.com>
2025-04-25 16:29:20 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 9.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,17 @@
+def solution(letter):
+    answer = ''
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'}
+    res = ''
+    for chr in letter:
+        if chr == " ":
+            answer += morse[res]
+            res = ''
+            continue
+        res += chr
+    answer += morse[res]
+    return answer
